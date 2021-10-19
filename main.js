d103c031af94366bc4066a5fe60788e77c89c508
emilywritescode <22375770+emilywritescode@users.noreply.github.com>
2021-10-19 19:40:23 -0400

    added commands, including to delete a poem

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
-const { app, BrowserWindow } = require('electron')
-const ipc = require('electron').ipcMain
+const { app, BrowserWindow, ipcMain } = require('electron')
 const dialog = require('electron').dialog
+const { execSync, spawnSync } = require('child_process')
+var config = require('./config')
 
 function createWindow () {
-    const wind = new BrowserWindow({
+    const win = new BrowserWindow({
         width: 1000,
         height: 750,
         webPreferences: {
@@ -13,18 +14,64 @@ function createWindow () {
         },
     })
 
-    wind.loadFile('index.html')
+    win.loadFile('index.html')
 }
 
-ipc.on('open-file-dialog', function (event, path) {
+ipcMain.on('open-file-dialog', (event, [path, command, args]) => {
     dialog.showOpenDialog({
         defaultPath: path,
         properties: ['openFile']
-    }).then(poem_file => {
-        console.log(poem_file.filePaths);
+    }).then(selected_file => {
+        ret = runShellCommand(createCommand(command, [selected_file.filePaths, args]));
+        event.returnValue = ret;
     });
 })
 
+
+function createCommand(command, args) {
+    let commandWithArgs = command
+    for (const arg of args) {
+        commandWithArgs += " " + arg
+    }
+    return commandWithArgs;
+}
+
+function runShellCommand(command) {
+    try {
+        let ret = execSync(command, { 'shell' : '/bin/zsh' })
+        return ret.toString();
+    } catch(err) {
+        console.log(err.stderr.toString());
+        return -1;
+    }
+}
+
+
+ipcMain.on('delete-poem-confirmation', (event, args) => {
+    let details = [];
+    details.push('POEMS: ' + args[0])
+    details.push('COLLECTIONS: ' + args[1])
+    details.push('FEATURES: ' + args[2])
+
+    let options = {
+        buttons: ["Yes" , "Cancel"],
+        message: 'Confirm you want to remove: ' + args[3],
+        detail: details.join('\n\n'),
+        checkboxLabel: 'Delete all associated features',
+        title: 'Confirm removing ' + args[3],
+        type: 'question'
+    }
+
+    dialog.showMessageBox(options).then((response) => {
+        if(response.response == 0) {
+            featoption = response.checkboxChecked ? "delete" : "keep";
+            ret = runShellCommand(createCommand(config.remove_poem_script, [args[4], "delete", featoption]))
+            console.log(ret);
+            return
+        }
+        return
+    })
+})
 
 app.whenReady().then(() => {
     createWindow()
--- renderer.js
+++ renderer.js
@@ -1,12 +1,32 @@
-const ipc = require('electron').ipcRenderer;
-var config = require('./config');
+const { ipcRenderer } = require('electron')
+var config = require('./config')
 
 document.getElementById('process-poem').onclick = () => {
     console.log('process poem!');
-    ipc.send('open-file-dialog', config.poems_folder);
+    ret = ipcRenderer.sendSync('open-file-dialog', [config.poems_folder, config.process_poem_script]);
 }
 
 document.getElementById('process-details').onclick = () => {
     console.log('process details!');
-    ipc.send('open-file-dialog', config.details_folder);
+    ret = ipcRenderer.sendSync('open-file-dialog', [config.details_folder, config.process_details_script]);
+}
+
+document.getElementById('create-collection').onclick = () => {
+    console.log('create collection!');
+    ret = ipc.send();
+}
+
+document.getElementById('delete-poem').onclick = () => {
+    console.log('remove poem!');
+    // First gather the effects of removing this poem
+    ret = ipcRenderer.sendSync('open-file-dialog', [config.poems_folder, config.remove_poem_script, "find"]);
+    // Pass data to confirmation modal
+    ret_array = ret.split('\n');
+    let d_poems, d_collections, d_features, poem_id;
+    [d_poems, d_collections, d_features] = (ret_array.slice(0,3)).map(x => x.slice(1,-1));
+    poem_id = ret_array[3]
+    poem_file = ret_array[4]
+    // Remove poem with confirmed options
+    ipcRenderer.sendSync('delete-poem-confirmation', [d_poems, d_collections, d_features, poem_id, poem_file])
+    return
 }
